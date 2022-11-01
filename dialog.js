df7a7a9a9e6fe7ffbffefdc192e54481438cc264
Sita Bérété <sita.berete.3@gmail.com>
2022-11-01 11:27:10 +0000

    Finalize

--- dialog.js
+++ dialog.js
@@ -1,14 +1,44 @@
 class Dialog {
   static _id = 0; // To uniquely identify a dialog and prevent conflict between instances.
 
+  static confirm() {
+    return new Promise((resolve, _) => {
+      const positiveAction = new DialogAction({
+        text: "Yes",
+        style: "success",
+      });
+
+      const negativeAction = new DialogAction({
+        text: "Cancel",
+        style: "critical",
+      });
+      const dialog = new Dialog({
+        message: "Are you sure you want to continue?",
+        actions: [positiveAction, negativeAction],
+      });
+
+      positiveAction.onClick = () => {
+        resolve(true);
+        dialog.hide();
+      };
+
+      negativeAction.onClick = () => {
+        resolve(false);
+        dialog.hide();
+      };
+
+      dialog.show();
+    });
+  }
+
   /**
    * Options:
    * {
-   * 
+   *
    *  actions: DialogActions[],
-   * 
+   *
    *  message: String
-   * 
+   *
    * }
    */
   constructor(options) {
@@ -28,26 +58,31 @@ class Dialog {
   _buildDialogHtml() {
     const dialogActions = this._buildDialogActions();
 
-    return htmlElementFromString(`
+    const dialogHtml = htmlElementFromString(`
       <div id="modal${this.id}" class="modal">
         <div class="dialog">
           <div class="dialog-content">
              ${this.options.message}
             <div class="dialog-actions">
-              ${dialogActions}
             </div>
           </div>
         </div>
       </div>
     `);
+
+    const dialogActionsContainer = dialogHtml.getElementsByClassName("dialog-actions").item(0);
+    dialogActions.forEach(action => {
+      dialogActionsContainer.appendChild(action);
+    });
+    return dialogHtml;
   }
 
   _buildDialogActions() {
     const actions = this.options.actions;
     if (actions) {
-      return actions.map(action => action.toString("html")).join("\n");
+      return actions.map(action => action.toHtmlElement());
     }
-    return "";
+    return [];
   }
 
   hide(destroyDialog = true) {
@@ -58,25 +93,23 @@ class Dialog {
     }
   }
 
-  destroy(){
+  destroy() {
     document.body.removeChild(this._htmlElement);
     this._htmlElement = null;
   }
-
 }
 
 class DialogAction {
-
   /**
    * Options:
    * {
-   * 
+   *
    *  text: string
-   * 
+   *
    *  style: "info" | "critical" | "success",
-   * 
+   *
    *  onClick: () -> void
-   * 
+   *
    * }
    */
   constructor(options) {
@@ -85,13 +118,15 @@ class DialogAction {
 
   toHtmlElement() {
     const htmlElement = htmlElementFromString(this._getHtmlString());
-    htmlElement.addEventListener("click", () => {
+    htmlElement.addEventListener("click", (e) => {
+      console.log("KKK",htmlElement);
+
       this.options.onClick && this.options.onClick();
     });
 
     return htmlElement;
   }
-  
+
   _getHtmlString() {
     return `
     <button class="button ${this.options.style ?? "info"}">
@@ -99,18 +134,17 @@ class DialogAction {
     </button>
   `;
   }
-    
+
   toString(stringType) {
     if (stringType == "html") {
       return this.toHtmlElement().outerHTML;
     }
     return `ModalAction:\n ${JSON.stringify(this)}`;
   }
-
 }
 
-function htmlElementFromString(htmlString){
-  const element = document.createElement('template');
+function htmlElementFromString(htmlString) {
+  const element = document.createElement("template");
   element.innerHTML = htmlString.trim();
   return element.content.firstElementChild;
 }
